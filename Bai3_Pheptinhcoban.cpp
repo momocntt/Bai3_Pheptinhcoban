dadac7e65930f75067c6721ad8b8a09a7f831cc9
momocntt <nguyenthihoaimo1207@gmail.com>
2024-11-04 14:36:34 +0700

    Add project files.

---
+++ Bai3_Pheptinhcoban.cpp
@@ -0,0 +1,14 @@
+// Bai3_Pheptinhcoban.cpp 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "CHUONG TRINH TINH CAC PHEP TOAN TINH CO BAN\n";
+    double so1, so2, tong, hieu, tich, thuong;
+    cout << "Nhap so 1:"; cin >> so1;
+    cout << "Nhap so 2:"; cin >> so2;
+    return 0;
+}
+
